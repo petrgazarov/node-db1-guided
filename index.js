6d55da5e7ecbde21157d77527c189a8bbe49c32d
Gabriel Cabrejas <gabriel@cabrejas.net>
2021-05-10 18:21:59 +0200

    wip

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ const server = require('./api/server.js')
 const PORT = 5000
 
 server.listen(PORT, () => {
-  console.log(`Listening on port ${PORT}...`)
+  console.log(`Listening on port ${PORT}...!!!!!`)
 })
